460c491227b09775603f2b68bd31557106b6d393
Francescosommellaa <franc.sommella@gmail.com>
2024-10-17 16:30:44 +0200

    Initialize

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/portfolio">Portfolio</router-link></li>
+        <li><router-link to="/contact">Contatti</router-link></li>
+      </ul>
+    </nav>
+  </template>
+  
+  <style scoped lang="scss">
+  @import '../assets/style/main.scss';
+  nav ul {
+    display: flex;
+    list-style: none;
+    background-color: $primary-color;
+  }
+  nav li {
+    margin: 0 15px;
+  }
+  </style>
