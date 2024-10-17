8f78e3e91cc537fb99af60bcbba91592526e06dd
Francescosommellaa <franc.sommella@gmail.com>
2024-10-17 17:07:28 +0200

    nav aggiornata

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,6 @@
   <nav>
     <ul>
       <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/portfolio">Portfolio</router-link></li>
-      <li><router-link to="/contact">Contatti</router-link></li>
     </ul>
   </nav>
 </template>
