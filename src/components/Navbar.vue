7eb05b831a9b13b058db94b942478c730515cba1
Francescosommellaa <franc.sommella@gmail.com>
2024-10-17 17:07:06 +0200

    initialize

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,18 @@
 <template>
-    <nav>
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/portfolio">Portfolio</router-link></li>
-        <li><router-link to="/contact">Contatti</router-link></li>
-      </ul>
-    </nav>
-  </template>
-  
-  <style scoped lang="scss">
-  @import '../assets/style/main.scss';
-  nav ul {
-    display: flex;
-    list-style: none;
-    background-color: $primary-color;
-  }
-  nav li {
-    margin: 0 15px;
-  }
-  </style>
+  <nav>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/portfolio">Portfolio</router-link></li>
+      <li><router-link to="/contact">Contatti</router-link></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped lang="scss">
+</style>
