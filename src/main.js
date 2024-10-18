5f06e88ae16336f46ee38ffd45900705304b8f62
Francescosommellaa <franc.sommella@gmail.com>
2024-10-18 09:39:46 +0200

    fix main.js Bootstrapdelete

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import '@/assets/style/main.scss'
-import 'bootstrap/dist/css/bootstrap.css'
 
 
 createApp(App).use(router).mount('#app');
